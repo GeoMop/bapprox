1b0cd607ac37a3146bc20333bec04b6d1d374f54
Jiri Hnidek <jiri.hnidek@tul.cz>
2015-10-27 10:44:45 +0100

    Added first version of terrain approximation using bspline surface.

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ B-Spline Approximator
 =====================
 
 Python tool for approximate 3d points using Bspline surface.
+
+Requirements
+------------
+
+* Python
+* Numpy
+* SciPy
+* GnuPlot
+
+How to Run
+----------
+
+    python ./src/main.py -f ./conf.yaml
---
+++ src/main.py
@@ -0,0 +1,28 @@
+import argparse
+
+import terrain_data
+
+def main(yaml_conf_filename):
+    """
+    Try to read data from all files
+    """
+    terrain = terrain_data.TerrainData(yaml_conf_filename)
+
+    terrain.load_conf_from_yaml()
+    terrain.load_terrain()
+    terrain.load_rivers()
+    terrain.load_area()
+
+    terrain.aproximate_terrain()
+    terrain.aproximate_2d_borders()
+    terrain.aproximate_2d_rivers()
+    
+    terrain.display_terrain()
+
+if __name__ == '__main__':
+    # Parse argument
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--filename", type=str,
+        help="Yaml conf file", default=None)
+    args = parser.parse_args()
+    main(args.filename)

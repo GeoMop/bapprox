159ed55193f9f8f988b89eb8311e043594d43519
Jiri Hnidek <jiri.hnidek@tul.cz>
2015-11-24 09:54:43 +0100

    Added more unit tests.

--- tests/test_terrain_data.py
+++ tests/test_terrain_data.py
@@ -4,9 +4,46 @@ This module includes unit tests of terrain_data module
 
 import terrain_data
 
-def test_new_terrain_data():
-	"""
-	Test of simple creating TerrainData object
-	"""
-	terrain = terrain_data.TerrainData("conf.yaml")
-	assert terrain != None
+def test_new_terrain_data_object():
+    """
+    Test of simple creating TerrainData object
+    """
+    terrain = terrain_data.TerrainData("conf.yaml")
+    assert terrain != None
+
+def test_load_yaml_conf():
+    """
+    Test loading configuration file from yaml file
+    """
+    terrain = terrain_data.TerrainData("conf.yaml")
+    terrain.load_conf_from_yaml()
+    assert terrain.conf.has_key('area') == True and \
+        terrain.conf.has_key('terrain') == True and \
+        terrain.conf.has_key('rivers') == True
+
+def test_load_terrain_data():
+    """
+    Simple test loading terrain data
+    """
+    terrain = terrain_data.TerrainData("conf.yaml")
+    terrain.load_conf_from_yaml()
+    terrain.load_terrain()
+    assert len(terrain.terrain_data) > 0
+
+def test_load_rivers_data():
+    """
+    Simple test loading rivers data
+    """
+    terrain = terrain_data.TerrainData("conf.yaml")
+    terrain.load_conf_from_yaml()
+    terrain.load_rivers()
+    assert len(terrain.rivers_data_2d) > 0
+
+def test_load_area_data():
+    """
+    Simple test loading area data
+    """
+    terrain = terrain_data.TerrainData("conf.yaml")
+    terrain.load_conf_from_yaml()
+    terrain.load_area()
+    assert len(terrain.area_borders_2d) > 0

2db01f256357a9bc15100463c77f6cfe5ad2b2c7
Jiri Hnidek <jiri.hnidek@tul.cz>
2016-01-11 15:13:43 +0100

    Added package convert and module bspline for converting data (scipy2occ).

---
+++ tests/convert/test_bspline.py
@@ -0,0 +1,18 @@
+import convert.bspline
+
+"""
+This modules is used for testing public functions and methods from
+module convert.bspline
+"""
+
+def test_approx_convertor_convert():
+	"""
+	Test public method convert() form class BSplineSurfConvertor
+	"""
+	scipy_bspline = [[0.0, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0],
+		[0.0, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0],
+		[0.0, 0.1, 0.0, 0.1, 0.3, 0.1, 0.0, 0.1, 0.0],
+		3, 3]
+	convertor = convert.bspline.BSplineSurfConvertor(scipy_bspline)
+	occ_bspline = convertor.convert()
+	assert occ_bspline is not None
